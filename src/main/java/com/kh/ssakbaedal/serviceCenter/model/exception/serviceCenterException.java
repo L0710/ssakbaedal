b132b1e6ad0832a91ba327d06b0af16fb409d0ff
L0710 <fodgud2001@gmail.com>
2020-10-22 20:38:47 +0900

    고객센터 FAQ 완료

---
+++ src/main/java/com/kh/ssakbaedal/serviceCenter/model/exception/serviceCenterException.java
@@ -0,0 +1,7 @@
+package com.kh.ssakbaedal.serviceCenter.model.exception;
+
+public class serviceCenterException extends RuntimeException {
+	public serviceCenterException(String msg) {
+		super(msg);
+	}
+}
